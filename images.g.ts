9eda552bf7109c235f2020421f5fbe0e1bd58a58
Mark20115363 <148377392+Mark20115363@users.noreply.github.com>
2023-10-21 18:22:41 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,190 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim0":
+            case "anim2":return [img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e 7 e 7 7 7 7 7 7 7 e 7 7 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+7 7 7 7 7 e e e 7 7 7 7 e e 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`];
+            case "myAnim1":
+            case "anim3":return [img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e 7 e 7 7 7 7 7 7 7 e 7 7 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+7 7 7 7 7 e e e 7 7 7 7 e e 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`];
+            case "myAnim2":
+            case "anim4":return [img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e 7 e 7 7 7 7 7 7 7 e 7 7 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+7 7 7 7 7 e e e 7 7 7 7 e e 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`];
+            case "myAnim3":
+            case "anim5":return [img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e 7 e 7 7 7 7 7 7 7 e 7 7 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+7 7 7 7 7 e e e 7 7 7 7 e e 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`];
+            case "myAnim4":
+            case "anim6":return [img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e 7 e 7 7 7 7 7 7 7 e 7 7 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+7 7 7 7 7 e e e 7 7 7 7 e e 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`];
+            case "car3Right":
+            case "anim1":return [img`
+. . . . e e e e e . . . . . . . 
+. . . e 2 2 2 2 2 e e . . . . . 
+. . e 2 2 2 2 2 2 2 e e . . . . 
+. e b 4 2 2 2 2 2 4 9 e . . . . 
+e b 9 4 2 2 2 2 4 4 9 9 e e . . 
+e 9 9 4 2 2 2 4 4 4 9 9 2 2 e . 
+e 9 9 2 4 4 4 4 4 2 9 9 2 2 2 e 
+e 9 9 e e e e e e e 9 9 2 2 2 e 
+e 9 b e b e b b b e b 9 2 2 2 e 
+e b e b b e b b b b e e e e 2 e 
+e e e 2 2 e 2 2 2 2 e e 3 3 e e 
+e e e e e e e e e e e e e 3 3 e 
+e e e e e e e e e e e e e e e e 
+e e f f f e e e e f f f e e e e 
+. f c c b f e e f c c b f e e . 
+. . f b b . . . . f b b . . . . 
+`, img`
+. . . . e e e e e . . . . . . . 
+. . . e 2 2 2 2 2 e e . . . . . 
+. . e 2 2 2 2 2 2 2 e e . . . . 
+. e b 4 2 2 2 4 4 4 9 e . . . . 
+e b 9 4 2 2 4 4 4 4 9 9 e e . . 
+e 9 9 4 2 4 4 4 4 4 9 9 2 2 e . 
+e 9 9 2 4 4 4 4 4 2 9 9 2 2 2 e 
+e 9 9 e e e e e e e 9 9 2 2 2 e 
+e 9 b e b e b b b e b 9 2 2 2 e 
+e b e b b e b b b b e e e e 2 e 
+e e e 2 2 e 2 2 2 2 e e 3 3 e e 
+e e e e e e e e e e e e e 3 3 e 
+e e e e e e e e e e e e e e e e 
+e e f f f e e e e f f f e e e e 
+. f b f f f e e f b f f f e e . 
+. . b b c . . . . b b c . . . . 
+`, img`
+. . . . e e e e e . . . . . . . 
+. . . e 2 2 2 2 2 e e . . . . . 
+. . e 2 2 2 2 2 2 2 e e . . . . 
+. e b 4 4 4 2 2 2 4 9 e . . . . 
+e b 9 4 4 4 2 2 2 4 9 9 e e . . 
+e 9 9 4 4 2 2 2 4 4 9 9 2 2 e . 
+e 9 9 2 4 4 4 4 4 2 9 9 2 2 2 e 
+e 9 9 e e e e e e e 9 9 2 2 2 e 
+e 9 b e b e b b b e b 9 2 2 2 e 
+e b e b b e b b b b e e e e 2 e 
+e e e 2 2 e 2 2 2 2 e e 3 3 e e 
+e e e e e e e e e e e e e 3 3 e 
+e e e e e e e e e e e e e e e e 
+e e f f f e e e e f f f e e e e 
+. f b b c f e e f b b c f e e . 
+. . f f f . . . . f f f . . . . 
+`, img`
+. . . . e e e e e . . . . . . . 
+. . . e 2 2 2 2 2 e e . . . . . 
+. . e 2 2 2 2 2 2 2 e e . . . . 
+. e b 4 2 2 2 2 2 4 9 e . . . . 
+e b 9 4 2 2 2 2 2 4 9 9 e e . . 
+e 9 9 4 2 2 2 2 4 4 9 9 2 2 e . 
+e 9 9 2 4 4 4 4 4 2 9 9 2 2 2 e 
+e 9 9 e e e e e e e 9 9 2 2 2 e 
+e 9 b e b e b b b e b 9 2 2 2 e 
+e b e b b e b b b b e e e e 2 e 
+e e e 2 2 e 2 2 2 2 e e 3 3 e e 
+e e e e e e e e e e e e e 3 3 e 
+e e e e e e e e e e e e e e e e 
+e e f f f e e e e f f f e e e e 
+. f c b b f e e f c b b f e e . 
+. . f f c . . . . f f c . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
